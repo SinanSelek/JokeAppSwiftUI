009f21b8c267b624d4a592dd183d99488d927d1d
Sinan Selek <sselek62@gmail.com>
2023-03-11 22:32:34 +0300

    Initial Commit

---
+++ JokeAppSwiftUI/JokeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeAppSwiftUIApp.swift
+//  JokeAppSwiftUI
+//
+//  Created by Sinan Selek on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct JokeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
